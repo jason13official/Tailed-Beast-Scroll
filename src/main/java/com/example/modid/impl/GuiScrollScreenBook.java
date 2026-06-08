392106992abb9a16c0f733cc40b0a434513e23fd
gandythewizard <gandythewizard@gmail.com>
2026-06-08 18:30:27 -0400

    start gui update

--- src/main/java/com/example/modid/impl/GuiScrollScreenBook.java
+++ src/main/java/com/example/modid/impl/GuiScrollScreenBook.java
@@ -1,7 +1,7 @@
 package com.example.modid.impl;
 
+import com.example.modid.Tags;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.narutomod.NarutomodMod;
 import net.narutomod.entity.EntityEightTails;
 import net.narutomod.entity.EntityFiveTails;
 import net.narutomod.entity.EntityFourTails;
@@ -30,24 +29,23 @@ public class GuiScrollScreenBook extends GuiScreen {
 
   public static final int GUIID = 0;
 
-  private static final ResourceLocation BOOK_GUI_TEXTURES = new ResourceLocation("textures/gui/book.png");
+  private static final ResourceLocation BOOK_GUI_TEXTURES = new ResourceLocation(Tags.MOD_ID, "textures/gui/background.png");
 
-  /** The X size of the inventory window in pixels. */
-  protected int xSize = 176;
-  /** The Y size of the inventory window in pixels. */
-  protected int ySize = 166;
-  /** Starting X position for the Gui. Inconsistent use for Gui backgrounds. */
+  protected int xSize = 320;
+  protected int ySize = 180;
   protected int guiLeft;
-  /** Starting Y position for the Gui. Inconsistent use for Gui backgrounds. */
   protected int guiTop;
-  /** The old x position of the mouse pointer */
   private float oldMouseX;
-  /** The old y position of the mouse pointer */
   private float oldMouseY;
+
+  private static final int ICON_SIZE = 28;
+  private static final int ICON_GAP = 3;
+  private static final int ICON_START_X = 171;
+  private static final int ICON_START_Y = 34;
 
   private EntityTailedBeast.Base[] tailedBeasts;
   private int selectedBeastIndex = 0;
-  private static final int PREVIEW_TARGET_HEIGHT = 50;
+  private static final int PREVIEW_TARGET_HEIGHT = 80;
 
   private final EntityPlayer editingPlayer;
   private final ItemStack scroll;
@@ -78,30 +76,38 @@ public class GuiScrollScreenBook extends GuiScreen {
         new EntityTenTails.EntityCustom(world)
       };
     }
-
-    this.buttonList.clear();
-    this.buttonList.add(new GuiButton(0, this.guiLeft + 30, this.guiTop + 75, 16, 20, "<"));
-    this.buttonList.add(new GuiButton(1, this.guiLeft + 130, this.guiTop + 75, 16, 20, ">"));
   }
 
   public void onGuiClosed() {
     Keyboard.enableRepeatEvents(false);
   }
 
+  private int[] getSlotPos(int index) {
+    int row = index < 9 ? index / 3 : 3;
+    int col = index < 9 ? index % 3 : 1;
+    return new int[]{ guiLeft + ICON_START_X + col * (ICON_SIZE + ICON_GAP),
+                      guiTop  + ICON_START_Y + row * (ICON_SIZE + ICON_GAP) };
+  }
+
   @Override
-  protected void actionPerformed(GuiButton button) {
-    if (this.tailedBeasts.length == 0) return;
-    if (button.id == 0) {
-      this.selectedBeastIndex = (this.selectedBeastIndex - 1 + this.tailedBeasts.length) % this.tailedBeasts.length;
-    } else if (button.id == 1) {
-      this.selectedBeastIndex = (this.selectedBeastIndex + 1) % this.tailedBeasts.length;
+  protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+    super.mouseClicked(mouseX, mouseY, mouseButton);
+    if (mouseButton == 0 && this.tailedBeasts != null && this.tailedBeasts.length > 0) {
+      for (int idx = 0; idx < this.tailedBeasts.length; idx++) {
+        int[] pos = getSlotPos(idx);
+        int sx = pos[0], sy = pos[1];
+        if (mouseX >= sx && mouseX < sx + ICON_SIZE && mouseY >= sy && mouseY < sy + ICON_SIZE) {
+          this.selectedBeastIndex = idx;
+          break;
+        }
+      }
     }
   }
 
   @Override
   public void handleMouseInput() throws IOException {
     super.handleMouseInput();
-    if (this.tailedBeasts.length > 0) {
+    if (this.tailedBeasts != null && this.tailedBeasts.length > 0) {
       int wheel = Mouse.getEventDWheel();
       if (wheel > 0) {
         this.selectedBeastIndex = (this.selectedBeastIndex - 1 + this.tailedBeasts.length) % this.tailedBeasts.length;
@@ -119,12 +125,32 @@ public class GuiScrollScreenBook extends GuiScreen {
 
     GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     this.mc.getTextureManager().bindTexture(BOOK_GUI_TEXTURES);
-    this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+    this.drawModalRectWithCustomSizedTexture(i, j, 0, 0, this.xSize, this.ySize, 480, 270);
 
-    if (this.tailedBeasts.length > 0) {
+    if (this.tailedBeasts != null && this.tailedBeasts.length > 0) {
+      // Large entity preview on left page
       EntityTailedBeast.Base beast = this.tailedBeasts[this.selectedBeastIndex];
       int scale = computePreviewScale(beast);
-      GuiInventory.drawEntityOnScreen(i + 51, j + 75, scale, (float)(i + 51) - this.oldMouseX, (float)(j + 75 - 50) - this.oldMouseY, beast);
+      GuiInventory.drawEntityOnScreen(i + 80, j + 130, scale,
+          (float)(i + 80) - this.oldMouseX, (float)(j + 80) - this.oldMouseY, beast);
+
+      // 10 icon slots on right page
+      for (int idx = 0; idx < this.tailedBeasts.length; idx++) {
+        int[] pos = getSlotPos(idx);
+        int sx = pos[0], sy = pos[1];
+
+        if (idx == this.selectedBeastIndex) {
+          drawRect(sx - 2, sy - 2, sx + ICON_SIZE + 2, sy + ICON_SIZE + 2, 0xFFFFDD00);
+        }
+        drawRect(sx - 1, sy - 1, sx + ICON_SIZE + 1, sy + ICON_SIZE + 1, 0xFF333333);
+        drawRect(sx, sy, sx + ICON_SIZE, sy + ICON_SIZE, 0x55000000);
+
+        EntityTailedBeast.Base thumb = this.tailedBeasts[idx];
+        int thumbScale = Math.max(1, (int)((ICON_SIZE - 8) / (thumb.height > 0 ? thumb.height : 1.0F)));
+        GuiInventory.drawEntityOnScreen(sx + ICON_SIZE / 2, sy + ICON_SIZE - 2, thumbScale,
+            (float)(sx + ICON_SIZE / 2) - this.oldMouseX,
+            (float)(sy + ICON_SIZE - 2 - 10) - this.oldMouseY, thumb);
+      }
     }
 
     super.drawScreen(mouseX, mouseY, partialTicks);
